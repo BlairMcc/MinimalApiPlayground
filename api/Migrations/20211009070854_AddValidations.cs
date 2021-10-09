41bbd7f95158c6cd21bafd9f8352e2f084062282
Blair McClelland <blair.mcclelland@alphacertlabs.com>
2021-10-09 20:30:12 +1300

    Add EF Core context, models, migrations. Add basic get/post api endpoints.

---
+++ api/Migrations/20211009070854_AddValidations.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class AddValidations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024,
+                oldNullable: true);
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,137 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(setup => setup.SwaggerDoc("v1", new OpenApiInfo()
+{
+    Description = "Product API using .Net 6 minimal web API, PostgreSQL, Docker Dev Environments",
+    Title = "Product API",
+    Version = "v1"
+})
+);
+builder.Services.AddValidatorsFromAssemblyContaining<Product>(lifetime: ServiceLifetime.Scoped);
+builder.Services.AddDbContext<ProductContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
+builder.Services.AddLogging();
 var app = builder.Build();
+
+await EnsureDb(app.Services, app.Logger);
+
+app.UseSwagger();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/product", async (ProductContext dbcontext) =>
+{
+    var products = await dbcontext.Products.ToListAsync();
+    app.Logger.LogInformation($"/product Count: {products.Count}");
+    return Results.Ok(products);
+});
+
+app.MapGet("/product/{id}", async (int id, ProductContext dbcontext) =>
+{
+    var result = await dbcontext.Products.FindAsync(id) is Product product ? Results.Ok(product) : Results.NotFound(id);
+    app.Logger.LogInformation($"/product/{id}:");
+    return result;
+});
+
+
+app.MapGet("/product/count", async (ProductContext dbcontext) =>
+{
+    var count = await dbcontext.Products.CountAsync();
+    app.Logger.LogInformation($"/product/count: {count}");
+    return Results.Ok(count);
+});
+
+app.MapGet("/product/search", async (string searchName, ProductContext dbcontext) =>
+{
+    var products = await dbcontext.Products
+        .Where(x => x.Name.Contains(searchName, StringComparison.InvariantCultureIgnoreCase))
+        .ToListAsync();
+    app.Logger.LogInformation($"/product/search Count: {products?.Count}");
+    return Results.Ok(products);
+});
+
+app.MapPost("/product", async (Product product, ProductContext dbcontext, IValidator<Product> validator) =>
+{
+    var validationResult = validator.Validate(product);
+    if (!validationResult.IsValid)
+    {
+        return Results.ValidationProblem(validationResult.ToDictionary());
+    }
+
+    dbcontext.Products.Add(product);
+    await dbcontext.SaveChangesAsync();
+    return Results.Created($"/product/{product.Id}", product);
+});
+
+app.UseSwaggerUI();
+
 app.Run();
+
+
+async Task EnsureDb(IServiceProvider services, ILogger logger)
+{
+    using (var db = services.CreateScope().ServiceProvider.GetRequiredService<ProductContext>())
+    {
+        logger.LogInformation("Ensuring database exists and is up to date at connection string '{connectionString}'");
+        await db.Database.EnsureDeletedAsync();
+        await db.Database.EnsureCreatedAsync();
+    }
+}
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+}
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(128)]
+    public string Name { get; set; }
+
+    [MaxLength(1024)]
+    public string Description { get; set; }
+
+    public class Validator : AbstractValidator<Product>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("name_required");
+            RuleFor(x => x.Name).MinimumLength(1).MaximumLength(128).WithMessage("name_minlength1_maxlength1024");
+            RuleFor(x => x.Description).MaximumLength(1024).WithMessage("description_maxlength1024");
+        }
+    }
+}
+
+public static class ValidationExtensions
+{
+    public static IDictionary<string, string[]> ToDictionary(this FluentValidation.Results.ValidationResult validationResult)
+    {
+        return validationResult.Errors
+                       .GroupBy(x => x.PropertyName)
+                       .ToDictionary(
+                           g => g.Key,
+                           g => g.Select(x => x.ErrorMessage).ToArray()
+                       );
+    }
+}
